7c652d7cfbb6339c5a9bd14c9d77337c56b33c94
Manan Goyal <goyalmanan960@gmail.com>
2025-02-07 10:27:59 +0530

    Solution to Calculate Average Temperature of a Week

---
+++ Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday;
+    scanf("%d %d %d %d %d %d %d",&Monday,&Tuesday,&Wednesday,&Thursday,&Friday,&Saturday,&Sunday);
+    printf("%.2f",(Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday)/7);
+    return 0;
+}
